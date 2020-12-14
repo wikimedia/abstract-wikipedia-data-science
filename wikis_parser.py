306f2e1464d4816df1fab30ec1c12bfd1f72c9f0
lost_enchanter <fufulomoffu@gmail.com>
2020-12-14 18:24:49 +0300

    Done some refactoring for shorter code and removing unnecessary constructions

--- wikis_parser.py
+++ wikis_parser.py
@@ -3,33 +3,36 @@ import requests
 
 WIKIMEDIA_WIKIS_PAGE = 'https://meta.wikimedia.org/wiki/Special:SiteMatrix'
 
-if __name__ == '__main__':
-    page = requests.get(WIKIMEDIA_WIKIS_PAGE)
-    soup = BeautifulSoup(page.content, 'html.parser')
 
-    # get first wikitable
-    table = soup.find('table', attrs={'id': 'mw-sitematrix-table'})
-
-    # removes all the crossed out wikis
+def parse_table(table):
+    # all the crossed out wikis
     deleted_links = table.find_all('del')
-    for match in deleted_links:
-        match.decompose()
 
-    # removes all the links to wikis, which don't exist (red link)
+    # all the links to wikis, which don't exist (red link)
     nonexisting_links = table.find_all('a', attrs={'class': 'new'})
-    for match in nonexisting_links:
-        match.decompose()
 
-    # removes links to "main hubs" for each type of wiki, they are in headers
+    # links to "main hubs" for each type of wiki, they are in headers
     col_name_links = table.find_all('th')
-    for match in col_name_links:
-        match.decompose()
 
+    for match in deleted_links + nonexisting_links + col_name_links:
+        match.decompose()
 
     links = []
     for elem in table.find_all('a', href=True):
         if elem.text:
             links.append('https:' + elem['href'])
+
+    return links
+
+
+if __name__ == '__main__':
+    page = requests.get(WIKIMEDIA_WIKIS_PAGE)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    main_table = soup.find('table', attrs={'id': 'mw-sitematrix-table'})    # get first wikitable
+    main_table_links = parse_table(main_table)
+
+
 
 
     print('Ok')
