96939f23ada831209a3736ce66f498bc716d14c0
lost_enchanter <fufulomoffu@gmail.com>
2020-12-14 17:18:17 +0300

    Added some comments

--- wikis_parser.py
+++ wikis_parser.py
@@ -7,9 +7,10 @@ if __name__ == '__main__':
     page = requests.get(WIKIMEDIA_WIKIS_PAGE)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    links = []
+    # get first wikitable
     table = soup.find('table', attrs={'id': 'mw-sitematrix-table'})
 
+    # removes all the crossed out wikis
     deleted_links = table.find_all('del')
     for match in deleted_links:
         match.decompose()
