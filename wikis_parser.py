d2c8206600b03fd70f8f296a340c072c5969445b
lost_enchanter <fufulomoffu@gmail.com>
2020-12-17 16:50:12 +0300

    Getting time of last update for metawiki

--- wikis_parser.py
+++ wikis_parser.py
@@ -1,6 +1,7 @@
 import toolforge
 
-CSV_FILENAME = 'wikipages.csv'
+CSV_LINKS = 'wikipages.csv'
+CSV_UPDATE_TIME = 'update_time.csv'
 
 
 def get_wikipages_from_db():
@@ -15,13 +16,31 @@ def get_wikipages_from_db():
         return cur.fetchall()
 
 
+def get_creation_date_from_db():
+    query = ("\n"
+             "    select create_time\n"
+             "    from INFORMATION_SCHEMA.TABLES\n"
+             "    where table_name = 'wiki'")
+
+    conn = toolforge.connect('meta')
+    with conn.cursor() as cur:
+        cur.execute(query)
+        print(cur.fetch())
+
+
 def save_links_to_csv(entries):
-    with open(CSV_FILENAME, 'w') as file:
+    with open(CSV_LINKS, 'w') as file:
         file.write('dbname,url\n')
         for entry in entries:
             file.write(entry[0] + ',' + entry[1] + '\n')
 
 
+def update_checker():
+    pass
+
+
+
 if __name__ == '__main__':
-    db_info = get_wikipages_from_db()
-    save_links_to_csv(db_info)
+    get_creation_date_from_db()
+    #db_info = get_wikipages_from_db()
+    #save_links_to_csv(db_info)
