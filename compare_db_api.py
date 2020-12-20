cc03ebfa4021abb1093ee6dd61db49a79aae4a4c
tanny411 <aysha.kamal7@gmail.com>
2020-12-20 16:34:50 +0600

    Optimize memory in comparison script

--- compare_db_api.py
+++ compare_db_api.py
@@ -6,10 +6,10 @@ csv.field_size_limit(sys.maxsize)
 
 ## Load DB CSV
 df = pd.read_csv('wiki_pages_db.csv', header=None, names=['page_id','page_title','page_latest','dbname'],
-                    dtype= {'page_id': int}, usecols=['page_id','page_title','dbname'])
+                    dtype= {'page_id': int}, usecols=['page_id','dbname'])
 df['dbname'] = df['dbname'].apply(lambda x: x[:-2])
 db_map = pd.read_csv('wikipages.csv')
-df = pd.merge(df, db_map, how='left', on='dbname')[['page_id', 'page_title', 'url']]
+df = pd.merge(df, db_map, how='left', on='dbname')[['page_id','url']]
 df['source'] = 'db'
 del db_map
 
@@ -18,20 +18,25 @@ print("Length of db pages:", len(df))
 
 ## load API CSV and delete duplicates
 len_api = 0
-for df_api in pd.read_csv('wiki_pages_api.csv', header=None, names=['page_id','page_title','url'], dtype= {'page_id': int}, chunksize=10):
+uniq_api = 0
+for df_api in pd.read_csv('wiki_pages_api.csv', header=None, names=['page_id' ,'page_title', 'url'], 
+                         chunksize=500, dtype={'page_id':int}, usecols=['page_id','url']):
     df_api['source'] = 'api'
     len_api += len(df_api)
     df = pd.concat([df, df_api])
-    df = df[~df.duplicated(subset=['page_id', 'page_title', 'url'], keep=False)]
+    df.drop_duplicates(subset=['page_id', 'url'], keep=False, inplace=True) # page titles do not match in API and db
+    df_api = df[df['source']=='api']
+    df = df[df['source']=='db']
+    uniq_api += len(df_api)
+    df_api.to_csv('comparison_api.csv', mode='a', header=False, index=False)
 
 print('Loaded API data...')
 print("Length of api pages:", len_api)
 
-print("Length of unique pages:", len(df))
-print("Length of unique pages in db:", len(df[df['source']=='db']))
-print("Length of unique pages in api:", len(df[df['source']=='api']))
+print("Length of unique pages in db:", len(df))
+print("Length of unique pages in api:", uniq_api)
 
-df.to_csv('comparison.csv')
+df.to_csv('comparison_db.csv', header=False, index=False)
 
 print("Ok")
 
