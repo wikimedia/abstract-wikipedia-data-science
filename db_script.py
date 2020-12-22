e1bb07f6a22ffbe9fbcd998215c6b4b9ef29f674
lost_enchanter <fufulomoffu@gmail.com>
2020-12-22 21:57:21 +0300

    Mirroring saving to database for db_scripts

--- db_script.py
+++ db_script.py
@@ -1,5 +1,35 @@
 import toolforge
 import pandas as pd
+import pymysql
+
+DATABASE_NAME = 's54588__data'
+
+
+def save_to_db(entries):
+    query = ("if not exists ("
+             "select 1 from Scripts where page_id = %s)"
+             "begin"
+             "insert into Scripts(dbname, page_id, title, touched, in_database)"
+             "             values(%s, %s, %s, %s, %s)"
+             "end"
+             "else"
+             "begin"
+             "update Scripts set in_database = %s where page_id = %s"
+             "end"
+             "endif;")
+    try:
+        conn = toolforge.toolsdb(DATABASE_NAME)
+        with conn.cursor() as cur:
+            for elem in entries:
+                cur.execute(query, elem['page_id'],
+                            elem['db'], elem['page_id'], elem['page_title'], elem['page_touched'], 1,
+                            1, elem['page_id'])
+        conn.commit()
+        conn.close()
+    except pymysql.err.OperationalError:
+        print('Wikiprojects update checker: failure, please use only in Toolforge environment')
+        exit(1)
+
 
 def encode_if_necessary(b):
     if type(b) is bytes:
@@ -7,12 +37,12 @@ def encode_if_necessary(b):
     return b
 
 ## Connect
-conn = toolforge.connect('meta')
+conn = toolforge.toolsdb(DATABASE_NAME)
 
 ## List databases
 dbs = []
 with conn.cursor() as cur:
-    cur.execute("show databases")
+    cur.execute("select dbnames from Sources where url is not NULL")    # all, except 'meta'
     for db in cur:
         dbs.append(db[0])
 
@@ -21,10 +51,13 @@ with conn.cursor() as cur:
     for db in dbs:
         cur.execute("use "+db)
         try:
-            SQL_Query = pd.read_sql_query("select page_id,page_title,page_latest  from page where page_content_model='Scribunto'", conn)
-            df_page = pd.DataFrame(SQL_Query, columns=['page_id','page_title','page_latest'])\
+            SQL_Query = pd.read_sql_query("select page_id, page_title, page_touched from page where page_content_model='Scribunto'", conn)
+            df_page = pd.DataFrame(SQL_Query, columns=['page_id', 'page_title', 'page_touched'])\
                 .applymap(encode_if_necessary)
             df_page['db'] = db
             df_page.to_csv('wiki_pages_db.csv', mode='a', header=False, index=False)
+
+            # saving to db
+            save_to_db()
         except:
             print('Error loading pages from db: ', db)
