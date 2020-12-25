722c3036e8462b270983270d1578f32c83b3c0fa
tanny411 <aysha.kamal7@gmail.com>
2020-12-25 20:12:04 +0600

    Minor bug and chore

--- get_db_pages.py
+++ get_db_pages.py
@@ -20,25 +20,11 @@ def get_only_db_pages():
         print('Failure: please use only in Toolforge environment')
         exit(1)
 
-def get_only_db_pages():
-    try:
-        conn = toolforge.toolsdb(DATABASE_NAME)
-        with conn.cursor() as cur:
-            SQL_Query = pd.read_sql_query("select page_id, title, dbname from Scripts where in_api = 0 and in_database = 1", conn)
-            df = pd.DataFrame(SQL_Query).applymap(encode_if_necessary)
-            df['wiki'] = df['dbname'].map(get_db_map(dbs=list(df['dbname'].values))[0])
-        conn.close()
-        return df
-    except pymysql.err.OperationalError:
-        print('Failure: please use only in Toolforge environment')
-        exit(1)
-
 def remove_redundant():
     try:
         conn = toolforge.toolsdb(DATABASE_NAME)
         with conn.cursor() as cur:
-            SQL_Query = pd.read_sql_query("delete from Scripts where in_api = 0 and in_database = 1", conn)
-            SQL_Query = pd.read_sql_query("delete from Scripts where in_api = 0 and in_database = 1", conn)
+            cur.execute("delete from Scripts where in_api = 0 and in_database = 1")
         conn.commit()
         conn.close()
         return df
--- test-files/compare_db_api.sh
+++
@@ -1,3 +0,0 @@
-cd abstract-wikipedia-data-science
-source venv/bin/activate
-python3 compare_db_api.py
