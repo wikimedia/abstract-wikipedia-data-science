f0d90ba5767bd6ebf527ce63380a44b2b58350c7
lost_enchanter <fufulomoffu@gmail.com>
2020-12-29 02:21:29 +0300

    Reworking fetch content to use utils

--- fetch_content.py
+++ fetch_content.py
@@ -7,20 +7,20 @@ import pymysql
 import numpy as np
 import argparse
 
+import utils.db_access as db_acc
 
 pymysql.converters.encoders[np.int64] = pymysql.converters.escape_int
 pymysql.converters.conversions = pymysql.converters.encoders.copy()
 pymysql.converters.conversions.update(pymysql.converters.decoders)
-
 
 ## define constants
 MIN_IDX = 0
 DATABASE_NAME = 's54588__data'
 
 
-def get_wiki_list(start_idx, end_idx):
+def get_wiki_list(start_idx, end_idx, user_db_port=None, user=None, password=None):
     try:
-        conn = toolforge.toolsdb(DATABASE_NAME)
+        conn = db_acc.connect_to_user_database(DATABASE_NAME, user_db_port, user, password)
         with conn.cursor() as cur:
             cur.execute("select url from Sources where url is not NULL")  # all, except 'meta'
             ret = [wiki[0] for wiki in cur][start_idx:end_idx + 1]
@@ -31,38 +31,41 @@ def get_wiki_list(start_idx, end_idx):
         exit(1)
 
 
-def save_content(wiki, data_list, in_api, in_database):
+def save_content(wiki, data_list, in_api, in_database, user_db_port=None, user=None, password=None):
+    data_df = pd.DataFrame(data_list,
+                           columns=['id', 'title', 'url', 'length', 'content', 'content_model', 'touched', 'lastrevid'])
 
-    data_df = pd.DataFrame(data_list, columns=['id', 'title', 'url', 'length', 'content', 'content_model', 'touched', 'lastrevid'])
-
-    query = ("insert into Scripts(dbname, page_id, title, sourcecode, touched, in_api, in_database, length, content_model, lastrevid, url) "
-             "             values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)\n"
-             "on duplicate key update title = %s, sourcecode = %s, touched = %s, in_api = %s, in_database = %s,"
-             "length = %s, content_model = %s, lastrevid = %s, url = %s, is_missed=%s"
-             )
+    query = (
+        "insert into Scripts(dbname, page_id, title, sourcecode, touched,"
+        " in_api, in_database, length, content_model, lastrevid, url) "
+        "             values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)\n"
+        "on duplicate key update title = %s, sourcecode = %s, touched = %s, in_api = %s, in_database = %s,"
+        "length = %s, content_model = %s, lastrevid = %s, url = %s, is_missed=%s"
+        )
     try:
-        conn = toolforge.toolsdb(DATABASE_NAME)
+        conn = db_acc.connect_to_user_database(DATABASE_NAME, user_db_port, user, password)
         with conn.cursor() as cur:
             cur.execute("select dbname from Sources where url = %s", wiki)
             dbname = cur.fetchone()[0]
             for index, elem in data_df.iterrows():
                 time = elem['touched'].replace('T', ' ').replace('Z', ' ')
                 cur.execute(query,
-                            [dbname, elem['id'], 
-                            elem['title'], elem['content'], time, in_api, in_database, elem['length'], elem['content_model'], elem['lastrevid'], elem['url'],
-                            elem['title'], elem['content'], time, in_api, in_database, elem['length'], elem['content_model'], elem['lastrevid'], elem['url'], 0])
+                            [dbname, elem['id'],
+                             elem['title'], elem['content'], time, in_api, in_database, elem['length'],
+                             elem['content_model'], elem['lastrevid'], elem['url'],
+                             elem['title'], elem['content'], time, in_api, in_database, elem['length'],
+                             elem['content_model'], elem['lastrevid'], elem['url'], 0])
         conn.commit()
         conn.close()
     except pymysql.err.OperationalError as err:
         print('Failure: please use only in Toolforge environment')
         exit(1)
     except Exception as err:
-        print('Error saving pages from',wiki)
+        print('Error saving pages from', wiki)
         print(err)
 
 
-def save_missed_content(wiki, missed):
-
+def save_missed_content(wiki, missed, user_db_port=None, user=None, password=None):
     missed_df = pd.DataFrame(missed, columns=['id'])
 
     query = ("insert into Scripts(dbname, page_id, in_api, is_missed) "
@@ -70,7 +73,7 @@ def save_missed_content(wiki, missed):
              "on duplicate key update in_api = %s, is_missed = %s"
              )
     try:
-        conn = toolforge.toolsdb(DATABASE_NAME)
+        conn = db_acc.connect_to_user_database(DATABASE_NAME, user_db_port, user, password)
         with conn.cursor() as cur:
             cur.execute("select dbname from Sources where url = %s", wiki)
             dbname = cur.fetchone()[0]
@@ -88,7 +91,7 @@ def needs_update(wiki, pageid, title, touched, revid):
     return True
 
 
-def get_contents(wikis, revise=False):
+def get_contents(wikis, revise=False, user_db_port=None, user=None, password=None):
     '''
     revise: `False` collects all contents and saves fresh
             `True` only collects those that have been edited
@@ -114,23 +117,23 @@ def get_contents(wikis, revise=False):
         _continue = ''
 
         while True:
-            params = {'action':'query',
-                    'generator':'allpages',
-                    'gapnamespace':828,
-                    'gaplimit':300,
-                    'format':'json',
-                    'prop':'info',
-                    'inprop':'url',
-                    'gapcontinue': _gapcontinue,
-                    'continue': _continue,
-                    }
-            
+            params = {'action': 'query',
+                      'generator': 'allpages',
+                      'gapnamespace': 828,
+                      'gaplimit': 300,
+                      'format': 'json',
+                      'prop': 'info',
+                      'inprop': 'url',
+                      'gapcontinue': _gapcontinue,
+                      'continue': _continue,
+                      }
+
             try:
                 result = session.get(params)
             except Exception as e:
                 print("Could not GET", wiki, "\n", e)
                 break
-            
+
             if 'query' in result.keys():
                 for page in list(result['query']['pages'].values()):
                     try:
@@ -140,54 +143,53 @@ def get_contents(wikis, revise=False):
                         length = page['length']
                         url = page['fullurl']
                         revid = page['lastrevid']
-                        
+
                         if (not revise) or needs_update(wiki, pageid, title, touched, revid):
-                            params = {'action':'query',
-                                    'format':'json',
-                                    'prop':'revisions',
-                                    'revids':revid,
-                                    'rvprop':'content',
-                                    'rvslots':'main',
-                                    'formatversion':2
-                                    }
-                    
+                            params = {'action': 'query',
+                                      'format': 'json',
+                                      'prop': 'revisions',
+                                      'revids': revid,
+                                      'rvprop': 'content',
+                                      'rvslots': 'main',
+                                      'formatversion': 2
+                                      }
+
                             rev_result = session.get(params)
 
                             content_info = rev_result['query']['pages'][0]['revisions'][0]['slots']['main']
                             content = content_info['content']
                             content_model = content_info['contentmodel']
-                            
-                            if content_model=='Scribunto':
+
+                            if content_model == 'Scribunto':
                                 data_list.append([pageid, title, url, length, content, content_model, touched, revid])
                     except Exception as err:
                         if 'pageid' in page.keys():
                             missed.append([pageid])
-                            print("Miss:", wiki, title, pageid, "\n" ,err)
-            
+                            print("Miss:", wiki, title, pageid, "\n", err)
+
                 cnt_data_list += len(data_list)
                 cnt_missed += len(missed)
-                save_missed_content(wiki, missed)
-                save_content(wiki, data_list, 1, 0)
+                save_missed_content(wiki, missed, user_db_port, user, password)
+                save_content(wiki, data_list, 1, 0, user_db_port, user, password)
                 print(cnt_data_list, 'pages loaded...')
                 data_list, missed = [], []
 
             try:
                 _continue = result['continue']['continue']
-                _gapcontinue = result['continue']['gapcontinue'] if 'gapcontinue' in  result['continue'] else ''
+                _gapcontinue = result['continue']['gapcontinue'] if 'gapcontinue' in result['continue'] else ''
             except:
                 break
 
         print("All pages loaded for %s. Missed: %d, Loaded: %d" \
-            %(wiki, cnt_missed, cnt_data_list))
-    
+              % (wiki, cnt_missed, cnt_data_list))
+
     print("Done loading!")
 
 
-def get_db_map(wikis=[], dbs=[]):
-    
+def get_db_map(wikis=[], dbs=[], user_db_port=None, user=None, password=None):
     query_input = []
-    
-    if len(wikis)>0:
+
+    if len(wikis) > 0:
         placeholders = ','.join('%s' for _ in wikis)
         query_input = wikis
         query = ("select dbname, url from Sources where url in (%s)" % placeholders)
@@ -195,23 +197,23 @@ def get_db_map(wikis=[], dbs=[]):
         placeholders = ','.join('%s' for _ in dbs)
         query_input = dbs
         query = ("select dbname, url from Sources where dbname in (%s)" % placeholders)
-    
+
     db_map = {}
 
     try:
-        conn = toolforge.toolsdb(DATABASE_NAME)
+        conn = db_acc.connect_to_user_database(DATABASE_NAME, user_db_port, user, password)
         with conn.cursor() as cur:
-            cur.execute(query,query_input)
-            db_map = {data[0]:data[1] for data in cur}
+            cur.execute(query, query_input)
+            db_map = {data[0]: data[1] for data in cur}
         conn.close()
     except pymysql.err.OperationalError as err:
         print('Failure: please use only in Toolforge environment')
         exit(1)
-    
+
     return db_map, placeholders
 
 
-def get_pages(df, in_api, in_database):
+def get_pages(df, in_api, in_database, user_db_port=None, user=None, password=None):
     '''
     df columns: page_id, dbname, wiki
     dbname not required
@@ -223,27 +225,27 @@ def get_pages(df, in_api, in_database):
         except Exception as e:
             print("Failed to connect to", wiki, "\n", e)
             continue
-            
+
         pageids = w_df['page_id'].values
         data_list = []
         missed = []
-        
+
         for pageid in list(pageids):
             params = {
-                'action':'query',
-                'format':'json',
-                'prop':'revisions|info',
-                'pageids':pageid,
-                'rvprop':'content',
-                'rvslots':'main',
-                'inprop':'url',
-                'formatversion':2
+                'action': 'query',
+                'format': 'json',
+                'prop': 'revisions|info',
+                'pageids': pageid,
+                'rvprop': 'content',
+                'rvslots': 'main',
+                'inprop': 'url',
+                'formatversion': 2
             }
 
             try:
                 result = session.get(params)
                 page = result['query']['pages'][0]
-                if page['lastrevid']!=0:
+                if page['lastrevid'] != 0:
                     url = page['fullurl']
                     title = page['title']
                     length = page['length']
@@ -252,28 +254,28 @@ def get_pages(df, in_api, in_database):
                     content_model = content_info['contentmodel']
                     touched = page['touched']
                     revid = page['lastrevid']
-                    
-                    if content_model=='Scribunto':
+
+                    if content_model == 'Scribunto':
                         data_list.append([pageid, title, url, length, content, content_model, touched, revid])
             except Exception as err:
                 missed.append([pageid])
                 print("Miss:", pageid, "from wiki:", wiki, "\n", err)
 
-        save_content(wiki, data_list, in_api, in_database)
-        save_missed_content(wiki, missed)
+        save_content(wiki, data_list, in_api, in_database, user_db_port, user, password)
+        save_missed_content(wiki, missed, user_db_port, user, password)
         print("All pages loaded for %s. Missed: %d, Loaded: %d" \
-            %(wiki, len(missed), len(data_list)))
+              % (wiki, len(missed), len(data_list)))
 
 
-def get_missed_contents(wikis):
-
+def get_missed_contents(wikis, user_db_port=None, user=None, password=None):
     print("Started loading missed contents...")
 
-    db_map, placeholders = get_db_map(wikis=wikis)
+    db_map, placeholders = get_db_map(wikis=wikis, user_db_port=user_db_port,
+                                      user=user, password=password)
     query = ("select page_id, dbname from Scripts where dbname in (%s) and in_api=1 and is_missed=1" % placeholders)
-    
+
     try:
-        conn = toolforge.toolsdb(DATABASE_NAME)
+        conn = db_acc.connect_to_user_database(DATABASE_NAME, user_db_port, user, password)
         with conn.cursor() as cur:
             cur.execute(query, list(db_map.keys()))
             df = pd.DataFrame(cur, columns=['page_id', 'dbname'])
@@ -330,7 +332,9 @@ if __name__ == "__main__":
 
     if not args.local:
         wikis = get_wiki_list(args.start_idx, args.end_idx)
-        get_contents(wikis)
+        get_contents(wikis, args.revise)
         get_missed_contents(wikis=wikis)
     else:
-        pass
+        wikis = get_wiki_list(args.start_idx, args.end_idx, args.user_db_port, args.user, args.password)
+        get_contents(wikis, args.revise, args.user_db_port, args.user, args.password)
+        get_missed_contents(wikis, args.user_db_port, args.user, args.password)
