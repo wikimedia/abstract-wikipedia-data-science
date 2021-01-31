eef03d6e306e6945a7fc0087af298c65a8b498c0
lost_enchanter <fufulomoffu@gmail.com>
2021-01-31 21:36:57 +0300

    Adding constants for analysis part

--- constants.py
+++ constants.py
@@ -1,1 +1,7 @@
 DATABASE_NAME = 's54588__data'
+
+ANALYSIS_BATCH_SIZE = 100
+# if there's more entries then batch size, how many times they should be shuffled and clustered
+ANALYSIS_SHUFFLE_AMOUNT = 10
+
+ANALYSIS_CLUSTERING_EPS = 0.05
