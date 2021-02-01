421e1f1658ad9c65446d0bce04fd65ecc2eee747
lost_enchanter <fufulomoffu@gmail.com>
2021-02-01 22:53:51 +0300

    Adding tables of eps, as "good" number changes depending on batch size

--- constants.py
+++ constants.py
@@ -6,4 +6,11 @@ ANALYSIS_SHUFFLE_AMOUNT = 10
 
 ANALYSIS_CLUSTERING_EPS = 0.05
 
+# table for batch size - eps
+CLUSTERING_EPS_TABLE = {
+    30: 0.2,
+    60: 0.1,
+    100: 0.05
+}
+
 ANALYSIS_TEST_OUT_FILE = 'clustering_test.md'
