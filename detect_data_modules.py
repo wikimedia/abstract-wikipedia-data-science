f11014761508c89db07c6ed3eafa63ec9cb78cd4
lost_enchanter <fufulomoffu@gmail.com>
2021-02-04 23:39:15 +0300

    Some tests to do this job with utils functions

--- detect_data_modules.py
+++ detect_data_modules.py
@@ -1,5 +1,44 @@
 import argparse
 
+from utils.db_query import query_data_generator, save_data, get_dbs
+
+
+def detect_data_modules(full_run=False, user_db_port=None, user=None, password=None):
+    query = (
+        "SELECT page_id, sourcecode"
+        "FROM Scripts "
+    )
+    new_values_query = "WHERE is_data IS NULL"
+    save_query = "UPDATE Scripts SET is_data=%s WHERE dbname=%s AND page_id=%s "
+
+    cols = ["page_id", "dbname", "sourcecode", "is_data"]
+    function_name = "detect_data_modules"
+
+    if full_run:
+        for df in query_data_generator(
+            query,
+            function_name,
+            cols,
+            replicas=False,
+            user_db_port=user_db_port,
+            user=user,
+            password=password
+        ):
+            print("tick")
+            #save_data(df, db, function_name, user_db_port, user, password)
+    else:
+        for df in query_data_generator(
+            query + new_values_query,
+            function_name,
+            cols,
+            replicas=False,
+            user_db_port=user_db_port,
+            user=user,
+            password=password
+        ):
+            print("tock")
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -42,3 +81,5 @@ if __name__ == "__main__":
         help="Toolforge password of the tool.",
     )
     args = parser.parse_args()
+
+    detect_data_modules(args.full_run, args.user_db_port, args.user, args.password)
