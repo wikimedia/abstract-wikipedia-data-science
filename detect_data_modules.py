7a97330317bbb09ee7c1cf82d7d565a4985c5ab2
lost_enchanter <fufulomoffu@gmail.com>
2021-02-05 22:18:57 +0300

    Added comments and formatting to follow PEP's guidelines

--- detect_data_modules.py
+++ detect_data_modules.py
@@ -1,10 +1,21 @@
 import argparse
 
-from utils.db_query import query_data_generator, save_data, get_dbs
+from utils.db_query import query_data_generator, save_data
 from utils.sourcecode_processing import remove_comments, check_if_data_function
 
 
 def detect_data_modules(full_run=False, user_db_port=None, user=None, password=None):
+    """
+    Gets sourcecodes of Lua functions from user's database and evaluates
+    whether function is considered to be 'data function'
+    (used only for storing information), saving it's results back to user's database.
+
+    :param full_run: determines whether to check all scripts or only unhandled ones.
+    :param user_db_port: port for connecting to local Sources table through ssh tunneling, if used.
+    :param user: Toolforge username of the tool.
+    :param password: Toolforge password of the tool.
+    :return: None
+    """
     query = (
         "SELECT page_id, dbname, sourcecode, is_data "
         "FROM Scripts "
@@ -18,13 +29,13 @@ def detect_data_modules(full_run=False, user_db_port=None, user=None, password=N
         query += new_values_query
 
     for df in query_data_generator(
-        query,
-        function_name,
-        cols,
-        replicas=False,
-        user_db_port=user_db_port,
-        user=user,
-        password=password
+            query,
+            function_name,
+            cols,
+            replicas=False,
+            user_db_port=user_db_port,
+            user=user,
+            password=password
     ):
         sourcecodes = df.loc[:, 'sourcecode']
         for i in range(sourcecodes.shape[0]):
@@ -41,16 +52,20 @@ def detect_data_modules(full_run=False, user_db_port=None, user=None, password=N
                 user,
                 password)
 
+    if full_run:
+        print("Done evaluating all Lua scripts content.")
+    else:
+        print("Done evaluating new Lua scripts content.")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Marks modules as data modules based on their sourcecode."
-        "By default is run only on Scripts, where is_data field in DB is not set."
-        "To use from local PC, provide all the additional flags needed for "
-        "establishing connection through ssh tunneling."
-        "More help available at "
-        "https://wikitech.wikimedia.org/wiki/Help:Toolforge/Database#SSH_tunneling_for_local_testing_which_makes_use_of_Wiki_Replica_databases"
+                    "By default is run only on Scripts, where is_data field in DB is not set."
+                    "To use from local PC, provide all the additional flags needed for "
+                    "establishing connection through ssh tunneling."
+                    "More help available at "
+                    "https://wikitech.wikimedia.org/wiki/Help:Toolforge/Database#SSH_tunneling_for_local_testing_which_makes_use_of_Wiki_Replica_databases"
     )
     parser.add_argument(
         "--full-run",
@@ -67,7 +82,7 @@ if __name__ == "__main__":
         type=int,
         default=None,
         help="Port for connecting to tables, created by user in Toolforge, "
-        "through ssh tunneling, if used.",
+             "through ssh tunneling, if used.",
     )
     local_data.add_argument(
         "--user",
