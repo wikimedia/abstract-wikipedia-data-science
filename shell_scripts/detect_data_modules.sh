cc0bbd6e4f3c3be9d82ff95bb1b5209de6360e21
lost_enchanter <fufulomoffu@gmail.com>
2021-02-08 23:41:48 +0300

    Updated shell script to be used for the full run also

--- shell_scripts/detect_data_modules.sh
+++ shell_scripts/detect_data_modules.sh
@@ -1,3 +1,9 @@
 cd abstract-wikipedia-data-science/
 source venv/bin/activate
-python3 detect_data_modules.py
+
+if [ $1 = "f" ]
+then
+    python3 detect_data_modules.py -f
+else
+    python3 detect_data_modules.py
+fi
