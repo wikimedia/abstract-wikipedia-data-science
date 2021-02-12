4d5e43371aa764ab469d877ab36f58f4809c1e75
tanny411 <aysha.kamal7@gmail.com>
2021-02-12 16:28:45 +0600

    Add word embedding based clustering

--- detect_similarity.py
+++ detect_similarity.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
+import re
 import argparse
 from scipy.sparse import vstack
 from sklearn.cluster import DBSCAN
 from sklearn.feature_extraction.text import TfidfVectorizer
-
+from gensim.models.fasttext import FastText
 from utils.db_query import close_conn, encode_if_necessary
 import utils.db_access as db_acc
 from constants import DATABASE_NAME
@@ -32,6 +33,40 @@ def get_tfidf(df, ngram=(3, 7)):
     return vectorizer.fit_transform(df["sourcecode"])
 
 
+def get_embedding(df, embedding_size=32, window_size=5, min_word=5, down_sampling=1e-2):
+    def preprocess_text(document):
+        pat = r"(?u)\w\w+|[^\w\s]+"
+        return re.findall(pat, document)
+
+    list_of_list = []
+    for i, code in df["sourcecode"].iteritems():
+        df.loc[i, "sourcecode"] = ""  ## to reduce memory footprint
+        list_of_list.append(preprocess_text(code))
+
+    ft_model = FastText(
+        list_of_list,
+        size=embedding_size,
+        window=window_size,
+        min_count=min_word,
+        sample=down_sampling,
+        sg=1,  ## skip gram
+        iter=100,
+    )
+
+    list_of_AV = []  ##Average
+    for doc in list_of_list:
+        AV = np.zeros(embedding_size)
+        for word in doc:
+            emb = ft_model.wv[word]
+            AV += emb
+        AV /= len(AV)
+        list_of_AV.append(AV)
+
+    del list_of_list
+
+    return np.array(list_of_AV)
+
+
 def find_clusters(df, X, eps=0.2, min_samples=2):
     clustering = DBSCAN(eps=eps, min_samples=min_samples).fit(X)
     return df.assign(group=clustering.labels_), clustering
@@ -44,7 +79,8 @@ def store_data(df, col):
 def get_similarity(with_data, user_db_port, user, password):
 
     df = get_data(with_data, user_db_port, user, password)
-    X = get_tfidf(df)
+    # X = get_tfidf(df)
+    X = get_embedding(df)
     del df["sourcecode"]
     df, clustering = find_clusters(df, X)
 
