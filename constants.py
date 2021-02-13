32ec390bd4f967ce4895d24468bc1fc3f4f23993
lost_enchanter <fufulomoffu@gmail.com>
2021-02-14 00:49:01 +0300

    Updating constants with ports value

--- constants.py
+++ constants.py
@@ -2,3 +2,16 @@ DATABASE_NAME = 's54588__data'
 
 NOC_DATABASES_DIRECTORY = 'https://noc.wikimedia.org/conf/dblists/'
 SHARDS_AMOUNT = 8
+
+DATABASE_SHARD_PORTS = {
+    1: 4711,
+    2: 4712,
+    3: 4713,
+    4: 4714,
+    5: 4715,
+    6: 4716,
+    7: 4717,
+    8: 4718
+}
+
+USER_DATABASE_PORT = 4710
