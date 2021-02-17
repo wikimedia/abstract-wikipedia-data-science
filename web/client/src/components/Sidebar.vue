87b15845d09ce348f10ba12206777d1c0db44664
lost_enchanter <fufulomoffu@gmail.com>
2021-02-17 23:03:07 +0300

    Added working "check all" and "uncheck all" functions

--- web/client/src/components/Sidebar.vue
+++ web/client/src/components/Sidebar.vue
@@ -1,6 +1,15 @@
 <template>
   <tbody>
     <div id="wiki-families">
+        <!-- Checkboxes list -->
+      <ul>
+        <li v-for='elem in projectFamilies' :key="elem">
+          <input type='checkbox' v-bind:value='elem' v-model='checkedProjectFamilies'
+                 @change='updateCheckallProjects()'> {{ elem }}
+        </li>
+      </ul>
+
+      <!--
       <input type="checkbox" id="wiki" value="Wikipedia" v-model="projectFamilies" />
       <label for="wiki">Wikipedia</label>
       <input type="checkbox" id="wiktionary" value="Wiktionary" v-model="projectFamilies" />
@@ -17,8 +26,14 @@
       <label for="wikiversity">Wikiversity</label>
       <input type="checkbox" id="wikisource" value="Wikisource" v-model="projectFamilies" />
       <label for="wikisource">Wikisource</label>
+      -->
+        <!-- Check All -->
+      <input type='checkbox' @click='checkAllProjects()'
+             v-model='projectFamiliesCheckAll' :disabled="projectFamiliesCheckAll == 1"> Check All
+      <input type='checkbox' @click='uncheckAllProjects()'
+             v-model='projectFamiliesUncheckAll' :disabled="projectFamiliesUncheckAll == 1"> Uncheck All
       <br />
-      <span>Checked families: {{ projectFamilies }}</span>
+      <span>Checked families: {{ checkedProjectFamilies }}</span>
     </div>
     <div id="data-modules">
       <input type="checkbox" id="checkbox" v-model="noDataModules" />
@@ -32,9 +47,53 @@ export default {
   name: "Sidebar",
   data() {
     return {
-      projectFamilies: [],
+      projectFamiliesCheckAll: false,
+      projectFamiliesUncheckAll: true,
+      projectFamilies: [
+        "Wikipedia",
+        "Wiktionary",
+        "Wikibooks",
+        "Wikiquote",
+        "Wikimedia",
+        "Wikinews",
+        "Wikiversity",
+        "Wikisource"
+      ],
+      checkedProjectFamilies: [],
       noDataModules: false
     }
+  },
+
+  methods: {
+    checkAllProjects: function () {
+      this.projectFamiliesCheckAll = !this.projectFamiliesCheckAll;
+      this.checkedProjectFamilies = []; // Check all
+      for (let key in this.projectFamilies) {
+        this.checkedProjectFamilies.push(this.projectFamilies[key]);
+      }
+      if (this.projectFamiliesUncheckAll === true) {
+        this.projectFamiliesUncheckAll = !this.projectFamiliesUncheckAll;
+      }
+    },
+    uncheckAllProjects: function () {
+      this.projectFamiliesUncheckAll = !this.projectFamiliesUncheckAll;
+      this.checkedProjectFamilies = [];
+      if (this.projectFamiliesCheckAll === true) {
+        this.projectFamiliesCheckAll = !this.projectFamiliesCheckAll;
+      }
+    },
+    updateCheckallProjects: function () {
+      if (this.projectFamilies.length == this.checkedProjectFamilies.length) {
+        this.projectFamiliesCheckAll = true;
+      } else {
+        this.projectFamiliesCheckAll = false;
+      }
+      if (this.checkedProjectFamilies.length == 0) {
+        this.projectFamiliesUncheckAll = true;
+      } else {
+        this.projectFamiliesUncheckAll = false;
+      }
+    },
   }
 }
 </script>
