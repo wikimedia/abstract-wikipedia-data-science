52760e8d960ba40c1b672bcb8751854ebc688d25
lost_enchanter <fufulomoffu@gmail.com>
2021-02-24 22:52:31 +0300

    Splitting setup into two parts as running code in Docker should be done separately

--- shell_scripts/web_toolforge_setup.sh
+++ shell_scripts/web_toolforge_setup.sh
@@ -7,18 +7,6 @@ cp -avr ../web/client/* $HOME/www/js/
 # run docker for js
 webservice --backend=kubernetes node10 shell
 
-# move to js folder
-cd $HOME/www/js
-
-# install dependencies
-npm install
-
-# build js
-npm run build
-
-# exit docker
-exit
-
 # copy dist folder
 cp -avp dist/ $HOME/www/python/src/client/
 
---
+++ shell_scripts/web_toolforge_setup_container.sh
@@ -0,0 +1,11 @@
+# move to js folder
+cd $HOME/www/js
+
+# install dependencies
+npm install
+
+# build js
+npm run build
+
+# exit docker
+exit
